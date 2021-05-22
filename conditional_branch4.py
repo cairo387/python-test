75420244c52cf44df6007ffd0eba8a78c9132589
cairo387 <mifuneri@outlook.jp>
2021-05-23 03:08:29 +0900

    [Test]条件分岐テスト２

---
+++ conditional_branch4.py
@@ -0,0 +1,7 @@
+score=70
+if score==100:
+  print('よくできました！')
+elif score>=60:
+  print('まずまずです')
+else:
+  print('頑張りましょう')
---
+++ conditional_branch5.py
@@ -0,0 +1,15 @@
+time=14
+if time>10 and time<18:
+  print('就業時間です')
+
+time=15
+if time==10 or time==15:
+  print('おやつの時間です')
+
+time=9
+if not time==18:
+  print('退社時間ではありません')
+
+time=14
+if 10<time<18:
+  print('就業時間です')
